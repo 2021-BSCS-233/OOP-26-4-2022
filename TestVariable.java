d2fcde4dbb5580b9cd81c73df929db942196d0cd
2021-BSCS-233 <104355118+2021-BSCS-233@users.noreply.github.com>
2022-04-27 18:58:17 +0500

    Add files via upload

---
+++ TestVariable.java
@@ -0,0 +1,27 @@
+class TestVariable
+{
+	public static void main(String arg[])
+	{
+		byte age;
+		age=20;
+		System.out.print("Age is "+age);
+		int Hight;
+		Hight=250;
+		System.out.print("\nHight is "+Hight+" cm");
+		short A;
+		A=120;
+		System.out.print("\nLength is "+A+" cm");
+		long Num;
+		Num=30034355;
+		System.out.print("\nNumber is "+Num);
+		float Value;
+		Value=20;
+		System.out.print("\nVariable is "+Value);
+		double Value2;
+		Value2=13420.34;
+		System.out.print("\nVariable is "+Value2);
+		char CH;
+		CH=97;
+		System.out.print("\nCharacter is "+CH);
+	}
+}
